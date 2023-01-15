973f2d119f9599c58549757a1513356603bf92df
pocho171 <32076706+pocho171@users.noreply.github.com>
2023-01-15 12:22:26 -0400

    Update contenido.js

--- contenido.js
+++ contenido.js
@@ -25,9 +25,9 @@ function observador(){
           contenido.innerHTML = `
           
           <div class="alert alert-warning alert-dismissible fade show mt-5" role="alert">
-  <strong>Hola invitado!</strong> Inicia sesión o <button data-toggle="modal" data-target="#exampleModal" ><u>Registrate</u></button> para ver el contenido. Clic aquí si <button data-toggle="modal" data-target="#exampleModal2" ><u>olvido su contraseña</u></button>
+  <strong>Hola invitado!</strong> Inicia sesiÃ³n o <button data-toggle="modal" data-target="#exampleModal" ><u>Registrate</u></button> para ver el contenido. Clic aquÃ­ si <button data-toggle="modal" data-target="#exampleModal2" ><u>olvido su contraseÃ±a</u></button>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">×</span>
+    <span aria-hidden="true">Ã—</span>
   </button>
 </div>
         
@@ -55,12 +55,12 @@ function aparece(user){
 <!--panel contenido inicio-->
 <div style="margin-top: 10px;"><div style="text-align: left; width: auto"><div class="panel panel-default"><div class="panel-body"><div style="text-align: center;"><span style="font-size: large;"><b>Aula Virtual</b></span></div><div style="margin-left: 10px; margin-right: 10px; text-align: justify;"><span style="font-size: medium;">
 </span>
-El Aula Virtual es una herramienta que brinda las posibilidades de realizar enseñanza en línea. Es un
+<p>El Aula Virtual es una herramienta que brinda las posibilidades de realizar enseÃ±anza en lÃ­nea. Es un
 entorno privado que permite administrar procesos educativos basados en un sistema de
-comunicación mediado por computadoras. De manera que se entiende como Aula Virtual, al espacio
-simbólico en el que se produce la relación entre los participantes en un proceso de enseñanza y
-aprendizaje que, para interactuar entre sí y acceder a la información relevante, utilizan
-prioritariamente un sistema de comunicación mediada por computadoras.
+comunicaciÃ³n mediado por computadoras. De manera que se entiende como Aula Virtual, al espacio
+simbÃ³lico en el que se produce la relaciÃ³n entre los participantes en un proceso de enseÃ±anza y
+aprendizaje que, para interactuar entre sÃ­ y acceder a la informaciÃ³n relevante, utilizan
+prioritariamente un sistema de comunicaciÃ³n mediada por computadoras.</p>
 </div></div></div></div></div><!--panel contenido fin--> 
 
 <!-- /* FIN DE LA ZONA VIP */ -->      
